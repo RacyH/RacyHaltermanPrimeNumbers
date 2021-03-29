ca0f4ae6f4aeee18c986e6d789f31c52add51382
RacyH <69870252+RacyH@users.noreply.github.com>
2021-03-29 10:51:11 -0500

    Add project files.

---
+++ RacyHaltermanPrimeNumbers/Source.cpp
@@ -0,0 +1,46 @@
+//Racy Halterman
+//Data Structures
+//Prime Numbers
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+bool checkprime(int input);
+
+int main()
+{
+	fstream data;
+	data.open("Prime.dat");
+
+	int max = 1000000;
+	bool p = false;
+	for (int i = 1; i < max + 1; i++)
+	{
+		p = checkprime(i);
+		if (p == true)
+		{
+			data << i;//this is not working.
+		}
+	}
+	data.close();
+}
+
+bool checkprime(int input)
+{
+	int x = (input/2) + 1;
+	if (input <= 3)//1, 2, and 3 are prime numbers
+	{
+		return true;
+	}
+
+	for (int i = 2; i < x; i++)
+	{
+		if (input % i == 0)
+		{
+			return false;
+		}
+	}
+	return true;
+}
